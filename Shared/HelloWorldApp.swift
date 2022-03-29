3b2d0721a1be8af75d717dd970ec47ca375b3ce2
aainaanadia97 <s2043599@siswa.um.edu.my>
2022-03-29 22:42:05 +0800

    Initial Commit

---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Aainaa Nadia on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
